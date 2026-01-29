35b5248b6f4b00bd5d73179c87259835a80a4759
Shaik Saifuddin <shaiksaifuddin2004@gmail.com>
2026-01-30 00:05:02 +0530

    Updated Codes

--- frontend/src/components/Message.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Message = ({ variant='info', children }) => (
-  <div className={`alert alert-${variant}`} role="alert">
-    {children}
-  </div>
-);
-
-export default Message;
