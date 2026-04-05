b6ee2b267e9b8ca91c2436b7046a1d55e23c4070
Shaik Saifuddin <shaiksaifuddin2004@gmail.com>
2026-04-05 15:11:45 +0530

    Frontend and backend after  Changes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -54,7 +54,7 @@ const Header = () => {
                     to="/orders"
                     className={location.pathname === "/orders" ? "active" : ""}
                   >
-                    My Orders
+                    My Orders <i className="fa-solid fa-cart-shopping ms-1"></i>
                   </Link>
                 </li>
 
